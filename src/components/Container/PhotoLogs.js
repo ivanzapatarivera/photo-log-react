e3dbba89a1c5db954b8f98774c0d789fa27931b1
Ivan Zapata <falenyo@gmail.com>
2020-12-09 02:50:43 -0500

    formatted JS

--- src/components/Container/PhotoLogs.js
+++ src/components/Container/PhotoLogs.js
@@ -1,5 +1,5 @@
 import "../../style.css";
-import Collage from './Collage'
+import Collage from "./Collage";
 
 function PhotoLogs() {
   return (
@@ -51,12 +51,8 @@ function PhotoLogs() {
           <h4 className="text-center pt-3" id="picturesCollageHeading">
             Recent PhotoLogs
           </h4>
-    
-            <Collage />
-            {/* <!-- First DIV --> */}
-            
-            
-       
+
+          <Collage />
         </div>
       </div>
 
@@ -76,7 +72,7 @@ function PhotoLogs() {
     if (logNewPhotoFrame.style.display === "flex") {
       logNewPhotoFrame.style.display = "none";
       logNewPhotoFrame.classList.remove("fade-in");
-      previewFileName.innerHTML = ""
+      previewFileName.innerHTML = "";
     } else {
       logNewPhotoFrame.style.display = "flex";
       logNewPhotoFrame.classList.add("fade-in");
@@ -89,9 +85,9 @@ function PhotoLogs() {
     var uploadBtnArr = uploadBtn.value + "";
     uploadBtnArr = uploadBtnArr.split("\\");
     var i = uploadBtnArr.length - 1;
-    var fileName = uploadBtnArr[i];   
+    var fileName = uploadBtnArr[i];
     console.log(uploadBtnArr);
-    console.log(fileName);    
+    console.log(fileName);
     console.log(`You've selected this file: `, uploadBtn.value);
     previewFileName.innerText = `Would you like to upload ${fileName} ?`;
   }
