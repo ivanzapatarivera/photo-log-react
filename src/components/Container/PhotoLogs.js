6f61949e0aec87d3d8ce678a97081a11078e9e47
Ivan Zapata <falenyo@gmail.com>
2020-12-08 21:14:55 -0500

    formatted JS

--- src/components/Container/PhotoLogs.js
+++ src/components/Container/PhotoLogs.js
@@ -4,10 +4,9 @@ function PhotoLogs() {
   return (
     //   <div className="row mx-auto p-1 logNewPhotoForm" id="logNewPhotoFrame">
     <div>
-      {/* <h1>Log Your Photo!</h1> */}
         <div class="col-12 col-md-6 mx-auto">
             <div class="card-body logNewPhotoFormDiv">
-            <h5 class="text-center">Log your Photo!</h5> 
+            <h5 class="text-center">Recent Photo Logs</h5> 
                 <form action="/upload" method="post" enctype="multipart/form-data">
                     <label class="btn btn-primary"> 
                         <i class="fa fa-image"></i> Photo
