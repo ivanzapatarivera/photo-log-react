f22790027bd7d38475ebd08586eb3f0690a1a36c
Ivan Zapata <falenyo@gmail.com>
2020-12-08 23:39:54 -0500

    status box show/hide OK

--- src/components/Container/StatusUpdate.js
+++ src/components/Container/StatusUpdate.js
@@ -20,12 +20,17 @@ const StatusUpdate = () => {
                 placeholder="What's on your mind?"
                 className="px-3 py-1"
               ></textarea>
-              <button className="button btn px-2 py-1" id="buttonSubmitStatus">
+              <button
+                className="button btn px-2 py-1"
+                id="buttonSubmitStatus"
+                onClick={hideStatusBox}
+              >
                 <i className="far fa-paper-plane"></i>
               </button>
               <span
                 className="buttonCancel btn py-1 px-2 ml-1"
                 id="buttonCancelStatus"
+                onClick={hideStatusBox}
               >
                 <i className="fas fa-times"></i>
               </span>
@@ -34,31 +39,29 @@ const StatusUpdate = () => {
         </div>
       </div>
       <div className="col-12 text-center">
-        <button className="px-4 py-1 postStatusBtn" id="postStatusBtn">
+        <button
+          className="px-4 py-1 postStatusBtn"
+          id="postStatusBtn"
+          onClick={showStatusBox}
+        >
           <i className="fas fa-feather-alt"></i>
         </button>
       </div>
     </div>
-  )
+  );
 
   // This section will display the status box
-  const statusBtn = document.querySelector("#postStatusBtn");
-  const buttonSubmit = document.querySelector("#buttonSubmitStatus");
-  const buttonCancel = document.querySelector("#buttonCancelStatus");
-  showHideStatusBox();
-  function showHideStatusBox() {
-    statusBtn.addEventListener("click", () => {
-      statusBox.style.display = "flex";
-      statusBox.classList.add("scale-in-center");
-    });
-    buttonSubmit.addEventListener("click", () => {
-      statusBox.style.display = "none";
-      statusBox.classList.remove("scale-in-center");
-    });
-    buttonCancel.addEventListener("click", () => {
-      statusBox.style.display = "none";
-      statusBox.classList.remove("scale-in-center");
-    });
+  function showStatusBox() {
+    const statusBox = document.querySelector("#statusBox");
+
+    statusBox.style.display = "flex";
+    statusBox.classList.add("scale-in-center");
+  }
+  function hideStatusBox() {
+    const statusBox = document.querySelector("#statusBox");
+
+    statusBox.style.display = "none";
+    statusBox.classList.remove("scale-in-center");
   }
 };
 
