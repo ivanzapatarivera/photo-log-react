d4ecb47b974d4ab9baf697997d1be9e31145073d
Ivan Zapata <falenyo@gmail.com>
2020-12-09 02:49:44 -0500

    collage OK

--- src/components/Container/Collage.js
+++ src/components/Container/Collage.js
@@ -2,55 +2,80 @@ import React, { useState, useEffect } from "react";
 import "../../style.css";
 
 const Collage = () => {
-  const [API, setAPI] = useState(null);
+  const [api, setApi] = useState(null);
   const fetchURL = "https://photo-logger.herokuapp.com";
-  const getAPI = () =>
-    fetch(`${fetchURL}/log`).then((res) => res.json());
+  const getApi = () => fetch(`${fetchURL}/log`).then((res) => res.json());
 
   useEffect(() => {
-    getAPI().then((API) => setAPI(API), console.log(API));
+    getApi().then((api) => setApi(api), console.log("This is API: "));
   }, []);
 
   return (
     <div className="row p-3 mainCollageDIV" id="collageDivs">
-      <div className="col-3 px-0">
-        <img
-          src="../image/Kyoto (Koyasu Pagoda - Kiyomizu).jpg"
-          id="5fce6905879a4c0015d5a2db"
-          onClick={showEventPicDiv}
-          className="collageImageDIV firstDiv"
-        />
-      </div>
+      {api?.map((item, index, array) =>
+        index === array.length - 6 ? (
+          <div className="col-3 px-0">
+            <img
+              src={fetchURL + item.URL.split("..").join("")}
+              id="5fce6905879a4c0015d5a2db"
+              onClick={showEventPicDiv}
+              className="collageImageDIV firstDiv"
+            />
+          </div>
+        ) : (
+          console.log(item)
+        )
+      )}
       <div className="col-5">
+        {api?.map((item, index, array) =>
+          index === array.length - 5 ? (
+            <div className="row">
+              {/* <!-- Second DIV --> */}
+              <div className="col-12 px-0">
+                <img
+                  src={fetchURL + item.URL.split("..").join("")}
+                  id="5fce6aa7879a4c0015d5a2ea"
+                  onClick={showEventPicDiv}
+                  className="collageImageDIV secondDiv"
+                />
+              </div>
+            </div>
+          ) : (
+            console.log(item)
+          )
+        )}
+
         <div className="row">
-          {/* <!-- Second DIV --> */}
-          <div className="col-12 px-0">
-            <img
-              src="../image/Maidono (Yasaka Jinja).jpg"
-              id="5fce6aa7879a4c0015d5a2ea"
-              onClick={showEventPicDiv}
-              className="collageImageDIV secondDiv"
-            />
-          </div>
-        </div>
-        <div className="row">
-          {/* <!-- Third DIV --> */}
           <div className="col-6 px-0">
-            <img
-              src="../image/Old Kyoto (Gion).jpg"
-              id="5fce6ae1879a4c0015d5a2f5"
-              onClick={showEventPicDiv}
-              className="collageImageDIV thirdDiv"
-            />
+            {/* <!-- Third DIV --> */}
+            {api?.map((item, index, array) =>
+              index === array.length - 4 ? (
+                <img
+                  src={fetchURL + item.URL.split("..").join("")}
+                  id="5fce6ae1879a4c0015d5a2f5"
+                  onClick={showEventPicDiv}
+                  className="collageImageDIV thirdDiv"
+                />
+              ) : (
+                console.log(item)
+              )
+            )}
           </div>
+
           {/* <!-- Fourth DIV --> */}
           <div className="col-6 px-0">
-            <img
-              src="../image/20200304_222707.jpg"
-              id="5fcf37ee9b56b600158d46f7"
-              onClick={showEventPicDiv}
-              className="collageImageDIV fourthDiv"
-            />
+            {api?.map((item, index, array) =>
+              index === array.length - 3 ? (
+                <img
+                  src={fetchURL + item.URL.split("..").join("")}
+                  id="5fcf37ee9b56b600158d46f7"
+                  onClick={showEventPicDiv}
+                  className="collageImageDIV fourthDiv"
+                />
+              ) : (
+                console.log(item)
+              )
+            )}
           </div>
         </div>
       </div>
@@ -58,23 +83,29 @@ const Collage = () => {
         <div className="row">
           {/* <!-- Fifth DIV --> */}
           <div className="col-12 px-0">
+          {api?.map((item, index, array) =>
+              index === array.length - 2 ? (
             <img
-              src="../image/20200303_134752.jpg"
+              src={fetchURL + item.URL.split("..").join("")}
               id="5fcf387a9b56b600158d470d"
               onClick={showEventPicDiv}
               className="collageImageDIV fifthDiv"
-            />
+            /> ):(console.log(item))
+              )}
           </div>
         </div>
         <div className="row">
           {/* <!-- Sixth DIV --> */}
           <div className="col-12 px-0">
+          {api?.map((item, index, array) =>
+              index === array.length - 1 ? (
             <img
-              src="../image/20180903_162515.jpg"
+              src={fetchURL + item.URL.split("..").join("")}
               id="5fcf39529b56b600158d471c"
               onClick={showEventPicDiv}
               className="collageImageDIV sixthDiv"
-            />
+            /> ):(console.log(item))
+          )}
           </div>
         </div>
       </div>
