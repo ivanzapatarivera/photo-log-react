064e7cf2bdc7771bab057f9cd2a7cb4c995242ae
Ivan Zapata <falenyo@gmail.com>
2020-12-08 23:56:06 -0500

    pictureDiv event handler OK

--- src/components/Container/PictureDiv.js
+++ src/components/Container/PictureDiv.js
@@ -3,10 +3,10 @@ import "../../style.css";
 const PictureDiv = () => {
   return (
     <div class="row">
-      <div
+      {/* <div
         class="col-12 col-md-12 mx-auto text-center fullScreenDiv"
         id="pictureDiv"
-      ></div>
+      ></div> */}
     </div>
   );
 };
