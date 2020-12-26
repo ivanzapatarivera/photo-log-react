9789f55be5eec7c6b7420cbc135558b869071705
Ivan Zapata <falenyo@gmail.com>
2020-12-26 12:26:49 -0500

    container

---
+++ src/components/Container.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Container() {
+    return (
+        <div className="container" style={{backgroundColor: "blue"}}>
+            <div className="row">
+                <div className="col-12">
+                    This is header
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Container;
