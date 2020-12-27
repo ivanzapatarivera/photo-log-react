bab193f64dd9a5d3db8c745b6bad89fd4cef567d
Ivan Zapata <falenyo@gmail.com>
2020-12-26 20:46:12 -0500

    connection with backend OK

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/photo-log
 // mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/photo-log-react`);
 
 app.use(bodyParser.json());
+app.get('/', (req, res) => {
+    return res.send('This is the Backend of PhotoLog.')
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
