cb539524efe32d86f3503b8e9d5028ff4ffd2748
Ivan Zapata <falenyo@gmail.com>
2020-12-26 23:13:37 -0500

    refactored JS

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-
-// IMPORT MODELS
-require('./models/profilepic');
-
+const express = require("express");
 const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
+// Importing models
+require("./models/profilepic");
+
+// Establishing mongoDB connection
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/photo-log-react`);
+mongoose.connect(
+  process.env.MONGODB_URI || `mongodb://localhost:27017/photo-log-react`
+);
 
 app.use(bodyParser.json());
 
-//IMPORT ROUTES
-require('./routes/productRoutes')(app);
+// Importing productRoutes
+require("./routes/productRoutes")(app);
 
-
-  const path = require('path');
-  app.use(express.static(path.join(__dirname, 'client/build')));
-
-  app.get('*', (req,res) => {
-    const index = path.join(__dirname, 'client', 'build', 'index.html');
-    res.sendFile(index);
-      // res.sendFile(path.join(__dirname, './client/build/index.html'))
-  })
-
+app.use(express.static(path.join(__dirname, "client/build")));
+app.get("*", (req, res) => {
+  const index = path.join(__dirname, "client", "build", "index.html");
+  res.sendFile(index);
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`app running on port ${PORT}`)
+  console.log(`app running on port http://localhost:${PORT}`);
 });
