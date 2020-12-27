8c9f35bee5c5000e734cd1ce03293826494e30b9
Ivan Zapata <falenyo@gmail.com>
2020-12-26 20:27:07 -0500

    initialized server OK

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/photo-log-react`);
+// mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/photo-log-react`);
+
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`App running on http://localhost:${PORT}`)
+});
