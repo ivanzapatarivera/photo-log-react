82994c3a0d6a847bcc4c1e9f7aae90642d990ab6
Ivan Zapata <falenyo@gmail.com>
2021-01-02 10:41:38 -0500

    formatted JS

--- models/pastries.js
+++ models/pastries.js
@@ -2,23 +2,23 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const pastries = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String, 
-        required: true
-    },
-    price: {
-        type: String,
-        required: true
-    }, 
-    imageURL: {
-        type: String,
-        required: false
-    }
-})
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: String,
+    required: true,
+  },
+  imageURL: {
+    type: String,
+    required: false,
+  },
+});
 
 const Pastries = mongoose.model("Pastries", pastries);
 module.exports = Pastries;
