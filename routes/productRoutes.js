aa63b96cacfe1eb7daafbfaa5ef5bf49a652b83e
Ivan Zapata <falenyo@gmail.com>
2021-01-02 10:59:30 -0500

    formatted JS

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,67 +1,62 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const ProfilePic = require("../models/profilepic");
 const Pastries = require("../models/pastries");
 
 module.exports = (app) => {
-
   app.get(`/api/pastries`, async (req, res) => {
     let pastries = await Pastries.find();
     return res.status(200).send(pastries);
-  })
+  });
 
   app.post(`/api/pastries`, async ({ body }, res) => {
-    let pastries = await Pastries.create(body)
-    return res => {
-      res.json().send({ pastries })
-      console.log(res.json())
-    }
-  })
-
+    let pastries = await Pastries.create(body);
+    return (res) => {
+      res.json().send({ pastries });
+      console.log(res.json());
+    };
+  });
 
   app.get(`/api/product`, async (req, res) => {
     let products = await ProfilePic.find();
     return res.status(200).send(products);
   });
 
-  app.post(`/api/product`, async ({body}, res) => {
+  app.post(`/api/product`, async ({ body }, res) => {
     // ProfilePic
     // .create(body)
     // .then((res) => res.json())
     // .catch((err) => {
     //   res.json(err);
-    // }) 
-    let product = await ProfilePic.create(body)
-    return res => res.json().send({ product })
-  
+    // })
+    let product = await ProfilePic.create(body);
+    return (res) => res.json().send({ product });
+
     // let product = await Product.create(req.body);
     // return res.status(201).send({
     //   error: false,
     //   product
     // })
-  })
+  });
 
   app.put(`/api/product/:id`, async (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
 
     let product = await Product.findByIdAndUpdate(id, req.body);
 
     return res.status(202).send({
       error: false,
-      product
-    })
-
+      product,
+    });
   });
 
   app.delete(`/api/product/:id`, async (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
 
     let product = await Product.findByIdAndDelete(id);
 
     return res.status(202).send({
       error: false,
-      product
-    })
-
-  })
-
-}
+      product,
+    });
+  });
+};
