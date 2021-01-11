adfe63b174240b057431917151113d2c893cd046
Ivan Zapata <falenyo@gmail.com>
2021-01-10 21:57:12 -0500

    corrected overflow

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Container from "./components/Container/Container";
 
 function App() {
 
   return(
     <div>
       <Header />
+      <Container />
       <Footer />
     </div>    
   )
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -21,7 +21,7 @@ export default class Header extends Component {
 
   render() {
     return (
-      <nav className="navbar shadow">
+      <nav className="navbar sticky-top shadow">
         <div className="container d-flex justify-content-center align-items-end">
           <img
             src={logo}
