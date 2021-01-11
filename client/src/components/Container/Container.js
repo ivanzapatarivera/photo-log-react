7371f7654249aca99b9e2f622783bd11acca9d71
Ivan Zapata <falenyo@gmail.com>
2021-01-10 22:26:47 -0500

    grid with sections ok

--- client/src/components/Container/Container.js
+++ client/src/components/Container/Container.js
@@ -2,11 +2,30 @@ import React, { Component } from "react";
 import "./style.css";
 
 export default class Container extends Component {
-    render() {
-        return(
-            <div className="container contents bg-warning">
-                This is container
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container contents bg-warning">
+        <div className="row">
+          <div className="profilePicture">
+            <section>Profile Picture</section>
+          </div>
+        </div>
+        <div className="row">
+          <div className="newUpdateMessage">
+            <section>Buttons for a new update message</section>
+          </div>
+        </div>
+        <div className="row">
+          <div className="gallery">
+            <section>Carrousel or Collage</section>
+          </div>
+        </div>
+        <div className="row">
+          <div className="updateMessages">
+            <section>Messages with Updates</section>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
