196bed5a2750d7f8cb1479f555353474cf9c27e4
Ivan Zapata <falenyo@gmail.com>
2021-01-10 22:37:59 -0500

    profile image container OK

--- client/src/components/Container/Container.js
+++ client/src/components/Container/Container.js
@@ -4,24 +4,33 @@ import "./style.css";
 export default class Container extends Component {
   render() {
     return (
-      <div className="container contents bg-warning">
+      <div className="container contents">
         <div className="row">
-          <div className="profilePicture">
-            <section>Profile Picture</section>
+          <div className="profilePicture col-12">
+            <section>
+                <div className="row">
+                    <div className="col-md-4 col-12 image">
+                        Image
+                    </div>
+                    <div className="col-md-8 col-12 description">
+                        Description
+                    </div>
+                </div>
+            </section>
           </div>
         </div>
         <div className="row">
-          <div className="newUpdateMessage">
+          <div className="newUpdateMessage col-12">
             <section>Buttons for a new update message</section>
           </div>
         </div>
         <div className="row">
-          <div className="gallery">
+          <div className="gallery col-12">
             <section>Carrousel or Collage</section>
           </div>
         </div>
         <div className="row">
-          <div className="updateMessages">
+          <div className="updateMessages col-12">
             <section>Messages with Updates</section>
           </div>
         </div>
