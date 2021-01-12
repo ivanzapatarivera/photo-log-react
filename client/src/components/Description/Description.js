7483d7735caeb7c267bf6ae0a61d533e9db7f154
Ivan Zapata <falenyo@gmail.com>
2021-01-11 22:19:39 -0500

    handle ()s OK

--- client/src/components/Description/Description.js
+++ client/src/components/Description/Description.js
@@ -2,10 +2,29 @@ import React, { Component } from "react";
 import "./style.css";
 
 export default class Description extends Component {
+  handleOnMouseOver = (e) => {
+    console.log(`You moved the mouse over ${e.target.innerText}`);
+  };
+
+  handleOnMouseOut = (e) => {
+    console.log(`You moved the mouse out of `, e.target.innerText);
+  };
+
+  handleOnClick = (e) => {
+    console.log(`You clicked on ${e.target.innerText}.`);
+  };
+
   render() {
     return (
       <div className="row">
-        <div className="col-12">Description Component</div>
+        <div
+          className="col-12"
+          onMouseOver={this.handleOnMouseOver}
+          onMouseOut={this.handleOnMouseOut}
+          onClick={this.handleOnClick}
+        >
+          Description Component
+        </div>
       </div>
     );
   }
