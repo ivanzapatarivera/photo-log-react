87021edfb1a69ba274f3a203b448ac7ef2b5ba96
Ivan Zapata <falenyo@gmail.com>
2021-01-15 14:12:31 -0500

    input btn woring

--- client/src/components/Forms/ImageUpload/ImageUpload.js
+++ client/src/components/Forms/ImageUpload/ImageUpload.js
@@ -23,35 +23,49 @@ export default class ImageUpload extends React.Component {
       .post("/upload", formData, config)
       .then(this.windowReload)
       .catch((error) => {});
-   
   }
 
   windowReload = () => {
     window.location.reload();
-  }
+  };
 
   onChange(e, filename, index, filenameArray, previewFileName) {
     this.setState({ file: e.target.files[0] });
-    filename = e.target.value
-    filenameArray = filename.split("\\")
-    index = filenameArray.length - 1
-    filename = filenameArray[index]
-    
+    filename = e.target.value;
+    filenameArray = filename.split("\\");
+    index = filenameArray.length - 1;
+    filename = filenameArray[index];
+
     previewFileName = document.querySelector("#previewFileName");
-    previewFileName.innerText = `Would you like to upload ${filename}?`
+    previewFileName.innerText = `Would you like to upload ${filename}?`;
   }
 
   render() {
     return (
-      <form onSubmit={this.onFormSubmit} className="mb-5 text-center col-12 col-md-4 p-3 mx-auto" style={{border: "1px solid lightgrey"}}>
+      <form
+        onSubmit={this.onFormSubmit}
+        className="mb-5 text-center col-12 col-lg-4 p-1 mx-auto"
+        style={{ border: "1px solid lightgrey" }}
+      >
         <h4>Upload a Picture</h4>
-        <label className="btn btn-secondary col-12 col-md-6 mb-2 mx-auto">Select Profile Photo
-          <input type="file" name="upload" className="btn btn-secondary" style={{display: "none"}} onChange={this.onChange} /><br />
+        <label className="btn btn-secondary col-12 col-md-6 mb-2 mx-auto">
+          Select Photo
+          <input
+            type="file"
+            name="upload"
+            style={{ display: "none" }}
+            onChange={this.onChange}
+          />
+          <br />
         </label>
         <div className="col-12 mx-auto">
-          <button type="submit" className="btn btn-primary mb-2">Upload</button>
-          &emsp;<button className="btn btn-danger mb-2">Cancel</button><br />
-          <label id="previewFileName" className=""></label><br />
+          <button type="submit" className="btn btn-primary mb-2">
+            Upload
+          </button>
+          &emsp;<button className="btn btn-danger mb-2">Cancel</button>
+          <br />
+          <label id="previewFileName" className=""></label>
+          <br />
         </div>
       </form>
     );
--- client/src/components/Forms/ProfilePictureUpload/ProfilePictureUpload.js
+++ client/src/components/Forms/ProfilePictureUpload/ProfilePictureUpload.js
@@ -3,40 +3,56 @@ import React, { Component } from "react";
 import "./style.css";
 
 export default class ProfilePictureUpload extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            file: null,
-        };
-        this.onFormSubmit = this.onFormSubmit.bind(this);
-        this.onChange = this.onChange.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: null,
+    };
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
 
-    onFormSubmit(e, res, formData, config) {
-        e.preventDefault();
-        formData = new FormData();
-        formData.append("profilepictures", this.state.file);
-        config = {
-            header: { "Content-Type": "multipart/form-data", },
-        };
-        axios.post("/profilepictures", formData, config)
-        .then(this.windowReload)
-        .catch((err) => {res.json(err)})
-    }
+  onFormSubmit(e, res, formData, config) {
+    e.preventDefault();
+    formData = new FormData();
+    formData.append("profilepictures", this.state.file);
+    config = {
+      header: { "Content-Type": "multipart/form-data" },
+    };
+    axios
+      .post("/profilepictures", formData, config)
+      .then(this.windowReload)
+      .catch((err) => {
+        res.json(err);
+      });
+  }
 
-    windowReload() {
-        window.location.reload();
-    }
+  windowReload() {
+    window.location.reload();
+  }
 
-    onChange(e) {
-        this.setState({ file: e.target.files[0] });
-    }
+  onChange(e) {
+    this.setState({ file: e.target.files[0] });
+  }
 
-    render() {
-        return(
-            <form onSubmit={this.onFormSubmit} className="col-12 col-md-4 p-3 mx-auto text-center profilePictureUploadForm" style={{border: "1px solid lightgrey", }}>
-                <h4>Update Profile Picture</h4>
-            </form>
-        )
-    }
+  render() {
+    return (
+      <form
+        onSubmit={this.onFormSubmit}
+        className="col-12 col-lg-4 p-1 mx-auto text-center profilePictureUploadForm"
+        style={{ border: "1px solid lightgrey" }}
+      >
+        <h4>Update Profile Picture</h4>
+        <label className="btn btn-secondary col-12 col-md-6 mb-2">
+          Select Photo
+          <input
+            type="file"
+            name="profilepictures"
+            className="profilePicturesInputBtn"
+            onChange={this.onChange}
+          ></input>
+        </label>
+      </form>
+    );
+  }
 }
