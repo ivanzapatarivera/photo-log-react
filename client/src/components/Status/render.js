e081ae771784287afaa6b7c6391f81803c0f387d
Ivan Zapata <falenyo@gmail.com>
2021-01-18 22:00:22 -0500

    formatted JS

--- client/src/components/Status/render.js
+++ client/src/components/Status/render.js
@@ -1,3 +1,7 @@
 import React, { useState, useEffect } from "react";
-
+import statusRenderService from "../../services/statusRenderService";
 import "./style.css";
+
+export default function RenderStatus() {
+
+}
