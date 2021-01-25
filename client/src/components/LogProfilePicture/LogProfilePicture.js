d7cdaefb7c070667231e3553f1d3032692238ecb
Ivan Zapata <falenyo@gmail.com>
2021-01-25 13:25:54 -0500

    removed redundant JS files

--- client/src/components/LogProfilePicture/LogProfilePicture.js
+++ client/src/components/LogProfilePicture/LogProfilePicture.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import ImageTag from "./ImageTag";
+import ImageTag from "../LogPhoto/ImageTag";
 import "./style.css";
 
 export default class LogProfilePicture extends Component {
